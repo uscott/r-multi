9edccb198235652ac78d6a16c41eca1f07a8dce3
urielscott <urielscott@d2a83d00-db20-4749-91b8-8355d7652aab>
2012-07-16 21:10:42 +0000

    Modify some files to get it to build, delete unnecessary

--- R/uri/trunk/r/zzz.R
+++ R/uri/trunk/r/zzz.R
@@ -1,14 +1,14 @@
 
 
-.First.lib = function(lib, pkg)
-{
-  try(library(chron))
-  try(library(tseries))
-  dyn.load("c:/r/library/uri/libs/uri.dll")
-}
+#.First.lib = function(lib, pkg)
+#{
+#  try(library(chron))
+#  try(library(tseries))
+#  dyn.load("c:/r/library/uri/libs/uri.dll")
+#}
 
 
-.Last.lib = function(lib, pkg)
-{
-  try(dyn.unload("c:/r/library/uri/libs/uri.dll"))
-}
+#.Last.lib = function(lib, pkg)
+#{
+#  try(dyn.unload("c:/r/library/uri/libs/uri.dll"))
+#}
