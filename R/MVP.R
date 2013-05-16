bbe21890d9f67d677507ef0c64da70601b386630
urielscott <urielscott@d2a83d00-db20-4749-91b8-8355d7652aab>
2013-05-16 03:39:41 +0000

    Minor changes to function "mvp"

--- R/MVP.R
+++ R/MVP.R
@@ -1,34 +1,25 @@
 ## Function for minimum variance portfolio calculation
 
-mvp = function( returns )
+mvp = function( returns, use = "everything" )
     ## returns is a matrix of asset returns with each column corresponding to an asset
 {
     N = ncol( returns )
 
     if ( N <= 0 )
-        return( numeric(0))
+        return( numeric( 0 ))
 
-    A = matrix( NA, N, N ) # covariance matrix
+    A = cov( returns, use = use ) # covariance matrix
 
-    for( i in 1:N )
-        for( j in 1:i )
-        {
-            A[ i, j ] = cov( returns[ , i ], returns[ , j ] )
-
-            if ( i != j )
-                A[ j, i ] = A[ i, j ]
-        }
-
-    Iden = matrix( 0, N, N ) # identity matrix
+    Id = matrix( 0, N, N ) # identity matrix
 
     for( i in 1:N )
-        Iden[ i, i ] = 1
+        Id[ i, i ] = 1
 
-    B    = solve( A, Iden ) # inverse of covariance matrix A
-    
+    B    = solve( A, Id ) # inverse of covariance matrix A
     ones = matrix( 1, N, 1 ) # column vector of 1s
+    w    = ( B %*% ones ) / as.double( t(ones) %*% B %*% ones ) # weights
 
-    w    = ( B %*% ones ) / as.double( t(ones) %*% B %*% ones )
-
+    rownames( w ) = colnames( returns )
+    
     return( w )
 }
