255705bb5a705aeb477d843c80fce3c8ee10eb9b
urielscott <urielscott@d2a83d00-db20-4749-91b8-8355d7652aab>
2014-11-13 21:39:39 +0000

    Write some C interpolation functions

---
+++ R/interp.R
@@ -0,0 +1,34 @@
+
+interp.lin = function( x.known, y.known, x.arg )
+{
+    if( length( x.known ) != length( y.known ))
+        stop( 'Length mismatch' )
+    
+    return( .Call(
+                'R_interp_lin',
+                as.double( x.known ),
+                as.double( y.known ),
+                as.double( x.arg ),
+                PACKAGE = 'uri' ))
+}
+
+
+interp.vol.sfc = function( t.in, m.in, v.in, t.arg, m.arg )
+{
+    if( length( t.in ) != length( m.in ) | length( m.in ) != length( v.in ))
+        stop( 'Length mismatch' )
+
+    if( length( t.arg ) == 1 & length( m.arg ) > 1 )
+        t.arg = rep( t.arg, length( m.arg ))
+    else if( length( m.arg ) == 1 & length( t.arg ) > 1 )
+        m.arg = rep( m.arg, length( t.arg ))
+    
+    return( .Call(
+                'R_interp_vol_sfc',
+                as.double( t.in ),
+                as.double( m.in ),
+                as.double( v.in ),
+                as.double( t.arg ),
+                as.double( m.arg )))
+}
+
